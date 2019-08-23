eeb66bac35133d36f1fd0b9289303ebaf7e6dc29
Dan Patrascu-Baba <dan@danpatrascu.com>
2019-08-23 21:24:50 +0300

    Add authors router

--- Module6-routing/final/src/app.js
+++ Module6-routing/final/src/app.js
@@ -1,47 +1,29 @@
+// Required modules
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const bookRouter = require('./routers/bookRouter');
+const authorRouter = require('./routers/authorRouter');
 
+// App constants
 const app = express();
 const port = process.env.PORT || 6501;
 
-app.use('/api/books', bookRouter.routes);
-
+// Middleware
 app.use(morgan('dev'));
 
-app.use("/", (req, res, next) => {
-  debug('Hello from first middleware');
-  next();
-});
+app.use('/api/books', bookRouter.routes);
+app.use('/api/authors', authorRouter.routes);
 
-app.use("/", (req, res, next) => {
-  debug('Hello from second middleware');
-  next();
-});
-
-app.use('/me', (req, res, next) => {
-  debug('Hello from single purpose middleware');
-  next();
-});
-
-app.use('/you', (req, res, next) => {
-  debug('Hello from YOU middleware');
-  next();
-});
-
+// Routes and controllers
 app.get('/', (req, res) => {
   debug('/ controller');
   res.sendFile(path.join(`${__dirname}\\index.html`));
 });
 
-app.get('/me', (req, res) => {
-  debug('/me controller');
-  res.send('This is from controller');
-});
-
+// Exporting app module
 module.exports.start = () => {
   app.listen(port, () => {
     debug(`Listening on port ${chalk.green(port)}`);
--- Module6-routing/final/src/routers/bookRouter.js
+++ Module6-routing/final/src/routers/bookRouter.js
@@ -1,21 +1,25 @@
+// Required modules
 const express = require('express');
 const morgan = require('morgan');
 
+// Constants
 const bookRouter = express.Router();
 
-bookRouter.use(morgan('combined'))
+// Middleware
+bookRouter.use(morgan('combined'));
 
+// Routes
 bookRouter.route('/')
   .get((req, res) => {
-    res.json( {books: 'several books'} );
+    res.json({ books: 'several books' });
   })
   .post((req, res) => {
-    res.json( {book: 'Book created'});
+    res.json({ book: 'Book created' });
   });
 
 bookRouter.route('/:id')
   .get((req, res) => {
-    res.json({title: 'Pro .Net Memory Management', author: 'Konrad Kokosa', yearPublished: 2018});
+    res.json({ title: 'Pro .Net Memory Management', author: 'Konrad Kokosa', yearPublished: 2018 });
   })
   .put((req, res) => {
     res.send(`Book with ID ${req.params.id} was updated`);
@@ -24,6 +28,5 @@ bookRouter.route('/:id')
     res.send(`Book with ID ${req.params.id} was deleted`);
   });
 
+// Module export
 module.exports.routes = bookRouter;
-
-
